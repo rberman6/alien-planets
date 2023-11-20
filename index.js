d8cd17b39f4979f297e73265549e7588cc3cfcf3
rberman6 <rittuberman@gmail.com>
2023-11-20 13:36:15 -0700

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("hi");
+
+import { alienPlanets } from "./alienPlanets.js";
+
+for (let planet of alienPlanets) {
+  console.log(planet);
+
+  const planetContainer = document.querySelector(".planet");
+
+  const planetDiv = document.createElement("div");
+  planetDiv.id = "planetDiv";
+  const planetName = document.createElement("h4");
+  const emoji = document.createElement("p");
+  emoji.id = "emoji";
+
+  planetName.textContent = `${planet.name}`;
+  emoji.textContent = `${planet.emoji}`;
+
+  planetContainer.appendChild(planetDiv);
+  planetDiv.appendChild(planetName);
+  planetDiv.appendChild(emoji);
+}
